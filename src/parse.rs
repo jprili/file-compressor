ff972b4f1a032ec86f9f7c6f0826c67bb463099c
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2025-08-05 20:03:59 -0700

    add parsing functionality

--- src/parse.rs
+++ src/parse.rs
@@ -1,9 +1,29 @@
-pub fn parse(mode: String, source: String, target: String) -> () {
-    println!("{mode}, {source}, {target}");
+use std::io;
+use std::fs::File;
+
+/**
+ * Parse the arguments.
+ *
+ * @param mode   Either compress or decompress.
+ * @param source The path to the source file.
+ * @param target The path to the target file.
+ * @panic when there is a problem reading the file.
+ */
+pub fn parse(mode: &String, source: &String, target: &String) {
+    let file = match File::open(source.as_str()) {
+        Ok(f)      => f,
+        Err(error) => panic!("ERROR: {:?}", error),
+    };
+
+    if mode.as_str() == "compress" {
+        compress(&file, target);
+    }
+    decompress(&file, target);
 }
 
-fn compress() -> () {
+fn compress(file: &File, target: &String) -> () {
+
 }
 
-fn decompress() -> () {
+fn decompress(file: &File, target: &String) -> () {
 }
