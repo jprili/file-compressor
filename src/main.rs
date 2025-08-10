bf69683f7c9c74f7e0f936f84c890ec4f420a7f7
James Paolo Rili <85321518+jprili@users.noreply.github.com>
2025-08-10 09:10:13 -0700

    fix conflicts

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+/*
+ * Module to compress and decompress a file.
+ *
+ * Uses basic Huffman encoding to compress.
+ */
+use std::env;
+use std::process::ExitCode;
+
+mod parse;
+
+const ARGUMENT_NUM: usize = 4;
+const ARG_START: usize = 1;
+const ARG_END:   usize = 3;
+
+/*
+ * Usage:
+ *  compress filename.extension compressed
+ *  decompress compressed filename.extension
+ */
+fn main() -> ExitCode {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != ARGUMENT_NUM {
+        eprintln!("Usage:");
+        eprintln!("    file-compressor [de]compress <source> <target>");
+        ExitCode::FAILURE
+    } else {
+        // TODO: figure this out
+        let [mode, source, destination] = args[ARG_START..=ARG_END];
+        parse::parse(mode, source, destination);
+        ExitCode::SUCCESS
+    }
+}
---
+++ src/parse.rs
@@ -0,0 +1,9 @@
+pub fn parse(mode: String, source: String, target: String) -> () {
+    println!("{mode}, {source}, {target}");
+}
+
+fn compress() -> () {
+}
+
+fn decompress() -> () {
+}
